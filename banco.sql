ba46819648124b3a18561bfb8c5b8674ebe11f81
KleitonLucarelli <146475976+KleitonLucarelli@users.noreply.github.com>
2023-10-03 15:50:39 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 03/10/2023 às 20:50
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `banco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+-- Erro ao ler a estrutura para a tabela banco.usuario: #1142 - Comando &#039;SHOW&#039; negado para o usuário &#039;&#039;@&#039;localhost&#039; na tabela `banco`.`usuario`
+-- Erro ao ler dados para tabela banco.usuario: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `banco`.`usuario`&#039; na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
